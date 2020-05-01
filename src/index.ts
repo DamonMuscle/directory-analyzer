cac6baf51539807a79aa74d5bcfa65df4efde8c5
DamonMuscle <damon.muscle@outlook.com>
2020-05-01 12:48:06 +0800

    refine powershell script path

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,25 @@ import chalk from "chalk";
 import * as shell from "shelljs";
 import { default as analyze } from "./analyze";
 
-const config: {
-  scanPath: string;
-  outputPath: string;
-} = require("../config.json");
+export default function analyzer(scanPath: string, outputPath: string) {
+  const psFile = path.resolve(__dirname, "stats.ps1");
 
-export default function analyzer() {
-  const psFile = path.resolve(process.cwd(), "src", "stats.ps1");
-
-  if (!fs.existsSync(config.outputPath)) {
-    fs.mkdirSync(config.outputPath);
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath);
   }
 
-  const temp = path.resolve(process.cwd(), "statsResult");
+  const temp = path.resolve(__dirname, "statsResult");
   if (!fs.existsSync(temp)) {
     fs.mkdirSync(temp);
   }
 
-  const command = `powershell.exe -ExecutionPolicy Unrestricted -File ${psFile} ${config.scanPath} ${temp}`;
+  const command = `powershell.exe -ExecutionPolicy Unrestricted -File ${psFile} ${scanPath} ${temp}`;
 
   console.log(chalk.blue("******************* Begin Scan **********************"));
 
   shell.exec(command, () => {
     console.log(chalk.yellow("******************* Begin Analyze **********************"));
-    analyze(temp, config.scanPath, config.outputPath);
+    analyze(temp, scanPath, outputPath);
     console.log(chalk.green("*********************** End ***************************"));
   });
 }
